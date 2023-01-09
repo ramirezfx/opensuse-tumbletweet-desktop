8b53514625fb557b0bdf76f9b9e90c978bff90fd
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-09 14:18:10 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+usermod -l $USER opensuse
+groupmod -n $USER opensuse
+usermod -d /home/$USER -m /home/opensuse
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
