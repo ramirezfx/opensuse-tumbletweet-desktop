d1f129035e7cacbdd7deb8d998c296bcf70c89c9
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-09 15:38:02 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
-usermod -l $USER opensuse
-groupmod -n $USER opensuse
-
+groupadd -r $USER -g 433 \
+&& useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
+&& mkdir /home/$USER \
+&& chown -R $USER:$USER /home/$USER \
+&& echo $USER':'$PASSWORD | chpasswd
+userdel -r opensuse
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
