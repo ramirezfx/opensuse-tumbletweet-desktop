44d673362c4c102f55021ab3cbc85c7129c0258e
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-16 08:15:54 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Add Language-Support
-CURRWINMANAGER=$(echo $XDG_CURRENT_DESKTOP)
-if [ CURRWINMANAGER == 'kde' ]
-then
-  wget -O /tmp/languages.txt https://github.com/ramirezfx/opensuse-tumbleweed-desktop/raw/main/languages-kde.txt && xargs -a /tmp/languages.txt zypper -n --no-gpg-checks in
-fi
-
 # Install Seafile-Cloud-Sync-Client
 # ---------------------------------
 zypper -n in seafile-client
