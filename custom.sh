0c5203c3df14c17dd4df04c65ea0629a6bf808e4
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-16 08:08:20 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -12,7 +12,7 @@ fi
 zypper -n in seafile-client
 
 # Install Visual Studio
------------------------
+# -----------------------
 # To Get the download-link, install the Video-Download-Helper-Plugin for your browser. Navigate to https://code.visualstudio.com.
 # Navigate to the bottom and dowload the arm-64 Version manually. Then use the Video-Download-Helper to get the link and paste it below:
 
